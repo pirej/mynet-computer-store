450569e0ee9ad4372f66374d447feff7a78446ae
Igor Risteski <pirej30@yahoo.com>
2022-06-05 03:11:40 +0200

    fetch the product details based on the slug

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -1,7 +1,142 @@
-import React from 'react';
+import { GraphQLClient, gql } from 'graphql-request';
+import styled from 'styled-components';
 
-const SlugPage = () => {
-  return <div>SlugPage</div>;
+const graphcms = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT, {
+  headers: {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN}`,
+  },
+});
+
+const ProductStyle = styled.div`
+  /* background-color: lightblue; */
+`;
+
+const SlugPage = ({ product }) => {
+  console.log('product is', product);
+  return <ProductStyle>SlugPage</ProductStyle>;
 };
 
 export default SlugPage;
+
+export async function getServerSideProps(context) {
+  const currentSlug = context.params.slug;
+
+  const query = gql`
+    query ($currentSlug: String!) {
+      cPUs(where: { slug: $currentSlug }) {
+        id
+        price
+        promotion
+        onDiscount
+        newproduct
+        discount
+        slug
+        stock
+        title
+        images {
+          url
+        }
+        brand
+        description {
+          raw
+        }
+        manufacturerLink
+        manufacturer
+        warranty
+      }
+      gPUs(where: { slug: $currentSlug }) {
+        id
+        price
+        promotion
+        onDiscount
+        newproduct
+        discount
+        slug
+        stock
+        title
+        images {
+          url
+        }
+        brand
+        description {
+          raw
+        }
+        manufacturerLink
+        manufacturer
+        warranty
+      }
+      laptops(where: { slug: $currentSlug }) {
+        id
+        price
+        promotion
+        onDiscount
+        newproduct
+        discount
+        slug
+        stock
+        title
+        images {
+          url
+        }
+        brand
+        description {
+          raw
+        }
+        manufacturerLink
+        manufacturer
+        warranty
+      }
+      motherboards(where: { slug: $currentSlug }) {
+        id
+        price
+        promotion
+        onDiscount
+        newproduct
+        discount
+        slug
+        stock
+        title
+        images {
+          url
+        }
+        brand
+        description {
+          raw
+        }
+        manufacturerLink
+        manufacturer
+        warranty
+      }
+      storage_Equipment(where: { slug: $currentSlug }) {
+        id
+        price
+        promotion
+        onDiscount
+        newproduct
+        discount
+        slug
+        stock
+        title
+        images {
+          url
+        }
+        brand
+        description {
+          raw
+        }
+        manufacturerLink
+        manufacturer
+        warranty
+      }
+    }
+  `;
+
+  const variables = { currentSlug };
+  const product = await graphcms.request(query, variables);
+
+  return {
+    props: {
+      product: product,
+    },
+  };
+}
