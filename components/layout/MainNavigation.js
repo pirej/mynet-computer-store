1f295f029292b8336e8b8f564af1e473894f79f2
Igor Risteski <pirej30@yahoo.com>
2022-06-24 01:27:56 +0200

    NavBar -- Display the number of items in cart

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,7 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { useProductContext } from '../../state/context/productContext';
 
 const Navigation = styled.div`
   background-color: #004695;
@@ -22,12 +23,61 @@ const Navigation = styled.div`
       }
     }
     .cartWrapper {
+      display: flex;
       padding-left: 3rem;
+
+      .fullCart {
+        display: flex;
+        align-items: center;
+        .items {
+          position: relative;
+          display: flex;
+          align-items: flex-start;
+          z-index: 100;
+          transform: translate(10px, -13px);
+          .numbers {
+            position: absolute;
+            z-index: 200;
+            width: 24px;
+            text-align: center;
+            p {
+              color: #e9edf2;
+              margin: 0;
+            }
+          }
+        }
+      }
+    }
+    .lessPadding {
+      padding-left: 1.5rem;
     }
   }
 `;
 
 const MainNavigation = () => {
+  const [loading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
+  const { cart } = useProductContext();
+
+  const allItemsFromCart = [];
+
+  if (!loading) {
+    cart.map(item => {
+      allItemsFromCart.push(item.numItems);
+    });
+  }
+
+  const initialAmount = 0;
+  const itemsInCart = allItemsFromCart.reduce(
+    (previousAmount, currentAmount) => previousAmount + currentAmount,
+    initialAmount
+  );
+
+  // console.log('itemsInCart', itemsInCart);
   return (
     <Navigation>
       <div className="topHeader">
@@ -49,15 +99,32 @@ const MainNavigation = () => {
               <Link href="/contact">contact</Link>
             </li>
           </ul>
-          <div className="cartWrapper">
+          <div className={`cartWrapper ${itemsInCart > 0 && 'lessPadding'}`}>
             <Link href="/cart">
               <a>
-                <Image
-                  src="/cartIcon.svg"
-                  height={24}
-                  width={24}
-                  alt="cartIcon"
-                />
+                <div className="fullCart">
+                  {itemsInCart > 0 && (
+                    <div className="items">
+                      <div className="numbers">
+                        <p>{itemsInCart}</p>
+                      </div>
+                      <Image
+                        src="/cartItems.svg"
+                        height={24}
+                        width={24}
+                        alt="itemsInCart"
+                      />
+                    </div>
+                  )}
+                  <div className="cart">
+                    <Image
+                      src="/cartIcon.svg"
+                      height={24}
+                      width={24}
+                      alt="cartIcon"
+                    />
+                  </div>
+                </div>
               </a>
             </Link>
           </div>
